5bf15bbd6662e793faa074d6fd153379d1d8d26a
huangjhLyl <646624437@qq.com>
2019-03-25 15:49:35 +0800

    增加admin-user模块的乐观锁插件和分页插件

--- admin-user/src/main/java/com/docker/adminuser/AdminUserApplication.java
+++ admin-user/src/main/java/com/docker/adminuser/AdminUserApplication.java
@@ -1,15 +1,36 @@
 package com.docker.adminuser;
 
+import com.baomidou.mybatisplus.extension.plugins.OptimisticLockerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaClient
+@MapperScan(value = "com.docker.adminuser.modules.*.mapper")
 public class AdminUserApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AdminUserApplication.class, args);
 	}
 
+    /**
+     * 分页插件
+     */
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        return new PaginationInterceptor();
+    }
+
+    /**
+     * 乐观锁插件
+     * @return
+     */
+    @Bean
+    public OptimisticLockerInterceptor optimisticLockerInterceptor() {
+        return new OptimisticLockerInterceptor();
+    }
 }
