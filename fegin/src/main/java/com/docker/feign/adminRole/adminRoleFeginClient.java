70e8ab893ba2229e4e4a26081953baf5e0a0e68a
huangjhLyl <646624437@qq.com>
2019-04-07 11:56:52 +0800

    集成feign-hystrix,实现短路功能

--- fegin/src/main/java/com/docker/feign/adminRole/adminRoleFeginClient.java
+++ fegin/src/main/java/com/docker/feign/adminRole/adminRoleFeginClient.java
@@ -2,14 +2,24 @@ package com.docker.feign.adminRole;
 
 import com.docker.feign.adminRole.entity.SysRole;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
-@FeignClient(name="adminRole")
+@FeignClient(name="adminRole",fallback = adminRoleFeginClient.adminRoleFeginClientFallBack.class)
 public interface adminRoleFeginClient {
 
 	@PostMapping("/sysRole/sys-role/findListByUserId")
 	public List<SysRole> findListByUserId(@RequestParam("userId") String userId);
+
+	@Component
+	static class adminRoleFeginClientFallBack implements adminRoleFeginClient{
+
+        @Override
+        public List<SysRole> findListByUserId(String userId) {
+            return null;
+        }
+    }
 }
---
+++ fegin/src/main/java/com/docker/feign/config/SybnHystrixFeignConfiguration.java
@@ -0,0 +1,19 @@
+//package com.docker.feign.config;//package com.docker.adminuser.common;
+//
+//import feign.Feign;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.context.annotation.Scope;
+//
+///**
+// * @author huangjh
+// * @date 2019/4/7 11:29
+// */
+//@Configuration
+//public class SybnHystrixFeignConfiguration {
+//    @Bean
+//    @Scope("prototype")
+//    public Feign.Builder feignHystrixBuilder() {
+//        return Feign.builder();
+//    }
+//}
